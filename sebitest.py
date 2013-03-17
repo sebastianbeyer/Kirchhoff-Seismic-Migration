3de7371479e23fcf448d18411b5eec1611ac0e03
Sebastian Beyer <sebastian.beyer@zmaw.de>
2013-03-17 16:45:04 +0100

    added first sulike plotting capabilities

--- sebitest.py
+++ sebitest.py
@@ -8,9 +8,10 @@ nsamples = 1001
 ntraces = 101
 noffsets = 5
 
+dt = 0.002
 
 
-f = open('SEIS-filt', 'r')
+f = open('SEIS-orig', 'r')
 
 # read data in ieee754 format
 inputs = np.fromfile(f, dtype=np.float32)
@@ -20,15 +21,20 @@ inputs = np.fromfile(f, dtype=np.float32)
 #data = inputs.reshape(nsamples,ntraces,noffsets)
 data = inputs.reshape(noffsets,ntraces,nsamples)
 
-x = np.linspace(1,nsamples,nsamples)
+x = np.linspace(0,nsamples*dt,nsamples)
+
+print x
 
 
-print data.shape
-print x.size
 
-#plt.plot(x,data[:,1,1],'-')
 
-plt.plot(data[1,2,:],x,'-')
+#plt.plot(data[1,1,:],x,'-')
+#plt.plot(data[1,2,:]+1,x,'-')
+
+for n in xrange(1, ntraces):
+    plt.plot(data[1,n,:]+n,x,'k-')
+
+plt.gca().invert_yaxis()
 plt.show()
 
 
